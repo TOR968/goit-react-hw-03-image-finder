62696f76ef64f78576b87022132911430aae0ac8
TOR968 <tor968@yahoo.com>
2020-12-13 18:16:19 +0200

    Update ImageGalleryItem.js

--- my-app/src/components/Components/ImageGallery/ImageGalleryItem/ImageGalleryItem.js
+++ my-app/src/components/Components/ImageGallery/ImageGalleryItem/ImageGalleryItem.js
@@ -1,45 +1,30 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import styles from '../../../utils/styles.module.css';
 import Modal from '../../Modal/Modal';
 import PropTypes from 'prop-types';
 
-class ImageGalleryItem extends Component({ url, alt, largeImageURL }) {
-  constructor(props) {
-    super(props);
-    this.state = {
-      IsModalOpen: false,
-    };
-  }
+const ImageGalleryItem = ({ url, alt, largeImageURL }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  openModal = () => {
-    this.setState({
-      IsModalOpen: true,
-    });
-  };
-  closeModal = () => {
-    this.setState({
-      IsModalOpen: false,
-    });
-  };
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
 
-  render() {
-    return (
-      <li className={styles.ImageGalleryItem}>
-        <img
-          src={url}
-          alt={alt}
-          className={styles['ImageGalleryItem-image']}
-          onClick={this.openModal}
-        />
-        {isModalOpen && (
-          <Modal closeModal={this.closeModal}>
-            <img src={largeImageURL} alt={alt} />
-          </Modal>
-        )}
-      </li>
-    );
-  }
-}
+  return (
+    <li className={styles.ImageGalleryItem}>
+      <img
+        src={url}
+        alt={alt}
+        className={styles['ImageGalleryItem-image']}
+        onClick={openModal}
+      />
+      {isModalOpen && (
+        <Modal closeModal={closeModal}>
+          <img src={largeImageURL} alt={alt} />
+        </Modal>
+      )}
+    </li>
+  );
+};
 
 ImageGalleryItem.propTypes = {
   url: PropTypes.string.isRequired,
