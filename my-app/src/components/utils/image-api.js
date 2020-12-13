fc1af9f88c7272c5a595c38df226a7e70fb37b2b
TOR968 <tor968@yahoo.com>
2020-12-13 19:00:40 +0200

    Update image-api.js

--- my-app/src/components/utils/image-api.js
+++ my-app/src/components/utils/image-api.js
@@ -1,20 +1,6 @@
 import KEY from './key';
-
-// function get(query, page = 1) {
-//   return fetch(
-//     `https://pixabay.com/api/?q=${query}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`,
-//   ).then(response => {
-//     if (response.ok) {
-//       return response.json();
-//     }
-
-//     return Promise.reject(new Error(`Нет картинки с именем ${query}`));
-//   });
-// }
-
-// export default get;
-
 import axios from 'axios';
+
 const get = (query, page = 1) => {
   const response = axios.get(
     `https://pixabay.com/api/?q=${query}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`,
@@ -23,23 +9,3 @@ const get = (query, page = 1) => {
 };
 
 export default get;
-/*
- */
-
-/* 
-function fetchPokemon(name) {
-  return fetch(`https://pokeapi.co/api/v2/pokemon/${name}`).then(response => {
-    if (response.ok) {
-      return response.json();
-    }
-
-    return Promise.reject(new Error(`Нет покемона с именем ${name}`));
-  });
-}
-
-const api = {
-  fetchPokemon,
-};
-
-export default api;
-*/
