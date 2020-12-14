612e5371fea6c74e3af67e4a93c060f7292e24b3
Maxim <Tor968@yahoo.com>
2020-12-14 11:36:40 +0200

    Update image-api.js

--- my-app/src/components/utils/image-api.js
+++ my-app/src/components/utils/image-api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const get = (query, page = 1) => {
   const response = axios.get(
-    `https://pixabay.com/api/?q=${query}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`,
+    `https://pixabay.com/api/?q=${query}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=10`,
   );
   return response;
 };
